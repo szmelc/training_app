cf4ea59608baa1092e2cef8eeb0c00d44c84fbe2
szmelc <lszmelc@gmail.com>
2018-01-30 22:19:21 +0100

    Add admin to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180129210716) do
+ActiveRecord::Schema.define(version: 20180130193906) do
 
   create_table "daily_posts_counts", force: :cascade do |t|
     t.date "date"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20180129210716) do
     t.string "avatar"
     t.string "phone_number"
     t.integer "posts_count", default: 0, null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
