b4bc414b759b617a40340a31c88577ff03f724f0
szmelc <lszmelc@gmail.com>
2018-02-08 22:50:49 +0100

    Fix worker

--- app/workers/post_request_worker.rb
+++ app/workers/post_request_worker.rb
@@ -1,16 +1,16 @@
 class PostRequestWorker < ApplicationMailer
   include Sidekiq::Worker
-  sidekiq_options :retry => 5
 
   sidekiq_retry_in do |count|
-    5
+    3
   end
 
   def perform(post_body, user_id)
-    HTTParty.post('http://localhost:9292/api/',
+    response = HTTParty.post('http://localhost:9292/api/',
     :query => {
       post_body: post_body,
       user_id: user_id
     })
+    fail if response.code != 201
   end
 end
